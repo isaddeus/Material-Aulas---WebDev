1a512577218edefa7563ca11d6da6f668bbe59ff
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 22:34:14 -0300

    CRUD

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,85 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+
+// ------------ OBJETOS --------------
+
+
+// const pessoa = {
+//     nome: "isabela",
+//     idade: 18,
+//     Empresas: [],
+//     isAdmin: true
+// }
+
+// console.log(pessoa.nome)
+// console.log(pessoa[idade])
+
+
+// ------ ---------- CRUD -----------------------------
+
+// Ao carregar a pagina, executa as linhas abaixo:
+
+window.onload = function(){
+    mostrarPosts();
+
+    // aqui, adicionamos o event listener pra receber o envio do formulario no CREATE
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+} 
+
+// ---------- CREATE ----------
+
+// PASSO A PASSO: 
+// 1- Ouvir o evento de envio de formulario
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagemPost = document.querySelector("#postImage").value;
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: new Date().toLocaleString() // NEW: Cria um novo objeto do tipo date ||| toLocaleString: Localiza de onde voce ta postando e deixa no formato regional 
+    }
+
+    posts.unshift(novoPost) // UNSHIFT: Adiciona no começo do código
+
+    mostrarPosts()
+}
+
+// READ,
+
+function mostrarPosts(){
+// PASSO A PASSO: 
+
+    // 1- Pegar o elemento onde os tweets serão inseridos
+    const listaPosts = document.querySelector("#postlist") // FUNCIONA COM BASE NOS SELETORES DO CSS ( . // #....)
+    listaPosts.innerHTML = ""
+
+    // 2- Percorrer o array pegando as informações, criando um tweet com cada uma delas
+    posts.forEach( pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+            <h2> ${pegaItem.text} </h2>
+            <img src="${pegaItem.image}"/>
+        `
+
+    // 3- Inserir no HTML
+        listaPosts.append(cardPost)
+    })
+
+}
+
+
+
+
+// UPDATE,
+function editarPosts(){}
+// DELETE
+function deletarPosts(){}
