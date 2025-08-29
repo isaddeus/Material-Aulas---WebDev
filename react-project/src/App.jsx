3078cbf8aa7dc3164aa024f5f7c9a62c1bf61ac0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-28 22:42:08 -0300

    REACT

---
+++ react-project/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+    <Header 
+      titulo="isabela"
+      texto="di deux"
+      cor="pink" />
+    <Header 
+      titulo="nicolaix"
+     texto="kimura haiiii"
+     cor="green"/>
+    <Header 
+      titulo="pedrinho cubinha >////<"
+      texto="choco minto yorimo a-na-ta"
+      cor="blue"/>
+    </>
+  )
+}
+
+export default App
