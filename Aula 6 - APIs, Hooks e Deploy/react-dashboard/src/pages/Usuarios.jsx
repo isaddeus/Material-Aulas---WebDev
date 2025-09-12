3c72145e38c29e2d5688c25e75807ceb7e6e6900
isaddeus <isadedeus0417@gmail.com>
2025-09-11 22:41:16 -0300

    API

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Usuarios.jsx
@@ -1,7 +1,31 @@
+import { data } from "react-router-dom";
+import { useEffect, useState} from "react";
+
+
+
 export default function Usuarios() {
+
+  const [usuarios, setUsuarios] = useState([]); // useState é usado para criar estado em componentes funcionais
+
+  useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/users") // FETCH é assíncrono, ou seja, não bloqueia o código
+  .then(response => response.json()) // .then() é usado para lidar com a resposta da requisição
+  .then(data => setUsuarios(data)) // .then() é usado para lidar com os dados retornados
+  .catch(erro => console.log(erro)) // .catch() é usado para lidar com erros na requisição
+  .finally(console.log("Carregamento finalizado")) // .finally() é usado para executar código após a requisição, independentemente do resultado
+  }, [])
+
+    // ; separa comandos em uma mesma linha, ou seja, é usado para separar instruções (acaba quebrando o processo)
+
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Usuários</h1>
+      {usuarios.map(pegaItem => (
+        <div key={pegaItem.id}>
+          <p>{pegaItem.name}</p>
+          <p>{pegaItem.email}</p>
+        </div>
+      ))}
       <p className="mt-2 text-gray-600">Página de usuários.</p>
     </div>
   );
